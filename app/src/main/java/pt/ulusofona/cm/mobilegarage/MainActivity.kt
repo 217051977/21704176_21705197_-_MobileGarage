2eb9ba7827bd1bd5f123670e610ad05b0ef45e81
217051977 <evoluid0r>
2020-04-17 16:40:49 +0100

    drawer done (ish)

--- app/src/main/java/pt/ulusofona/cm/mobilegarage/MainActivity.kt
+++ app/src/main/java/pt/ulusofona/cm/mobilegarage/MainActivity.kt
@@ -1,12 +1,29 @@
 package pt.ulusofona.cm.mobilegarage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_home_menu.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_log_out -> finish()
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        }
+        super.onBackPressed()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +37,24 @@ class MainActivity : AppCompatActivity() {
 //        set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_home_menu)
         setSupportActionBar(toolbar)
+        setupDrawerMenu()
     }
+
+    private fun setupDrawerMenu() {
+
+        val toggle = ActionBarDrawerToggle(
+
+            this,
+            drawer,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+
+        )
+        nav_drawer.setNavigationItemSelectedListener(this)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
 }
