8b65ac3e4ddc14ac5a7c852be75f2c39fd2f7a80
Bruno Leal <leal.bruno997@gmail.com>
2020-04-29 19:32:29 +0100

    update

--- app/src/main/java/pt/ulusofona/cm/mobilegarage/ui/fragments/ParkMeNowFragment.kt
+++ app/src/main/java/pt/ulusofona/cm/mobilegarage/ui/fragments/ParkMeNowFragment.kt
@@ -1,7 +1,28 @@
 package pt.ulusofona.cm.mobilegarage.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import butterknife.ButterKnife
+import pt.ulusofona.cm.mobilegarage.R
 
 class ParkMeNowFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.fragment_park_me_now,
+            container,
+            false
+        )
+
+        ButterKnife.bind(this, view)
+        return view
+    }
 
 }
