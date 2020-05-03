9b0425c9dfa88a87af04c4173a039428827b2bcc
Bruno Leal <leal.bruno997@gmail.com>
2020-05-03 12:25:09 +0100

    swipe done

---
+++ app/src/main/java/pt/ulusofona/cm/mobilegarage/SwipeCallback.kt
@@ -0,0 +1,24 @@
+//package pt.ulusofona.cm.mobilegarage
+//
+//import androidx.recyclerview.widget.ItemTouchHelper
+//import androidx.recyclerview.widget.RecyclerView
+//import pt.ulusofona.cm.mobilegarage.ui.adapters.FavoritesAdapter
+//
+//class SwipeCallback(private val myAdapter: FavoritesAdapter) : ItemTouchHelper.SimpleCallback {
+//
+//    init {
+//        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT)
+//    }
+//
+//    override fun onMove(
+//        recyclerView: RecyclerView,
+//        viewHolder: RecyclerView.ViewHolder,
+//        target: RecyclerView.ViewHolder
+//    ): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//        TODO("Not yet implemented")
+//    }
+//}
